bfd8bb9ab084c22be42111459f1e7b76a6e4115f
hollyk24 <keir8638@vandals.uidaho.edu>
2021-09-29 21:57:07 -0700

    Code Update

---
+++ Them's fightin' words/Assets/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame(){
+        //SceneManager.LoadScene("Game") will have to have updated scene to load
+        Debug.Log("Starting Game");
+    }
+}
