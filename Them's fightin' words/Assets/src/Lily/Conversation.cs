feaf752b1955fefaf69e88aa5fbac11a392336e8
lily <lilymason@msn.com>
2021-09-30 00:10:52 -0700

    NPC temp Dialog

--- Them's fightin' words/Assets/src/Lily/Conversation.cs
+++ Them's fightin' words/Assets/src/Lily/Conversation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 /*
  * lily Mason
  * This class is to be added to any NPC that one has a conversation with
@@ -13,16 +14,22 @@ public class Conversation : MonoBehaviour {
     [Header("References")]
     [Tooltip("The popup when the player is in range in order to tell them what button they should press")]
     public GameObject popUp;
+    public GameObject chatUI;
+    
+    [Header("Temp Vars")]
+    public string dialog;
+    public Text text;
 
     private Controls controls;
     private InputAction action;
     #endregion
     #region UNITY
     private void Start() {
+        //Temp need Gamemanager should be apart of main system
         controls = new Controls();
         action = controls.Player.Interact;
 
-        action.performed += Call;
+        action.performed += EnableUI;
         action.Disable();
     }
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -32,11 +39,19 @@ public class Conversation : MonoBehaviour {
     private void OnTriggerExit2D(Collider2D collision) {
         popUp.SetActive(false);
         action.Disable();
+        chatUI.SetActive(false);
     }
     #endregion
     #region
-    public void Call(InputAction.CallbackContext obj) {
-        popUp.SetActive(false);
+    public void EnableUI(InputAction.CallbackContext obj) {
+        if (chatUI.activeInHierarchy) {
+            OnTriggerExit2D(null);
+        }
+        else {
+            popUp.SetActive(false);
+            text.text = dialog;
+            chatUI.SetActive(true);
+        }
     }
     #endregion
 }
