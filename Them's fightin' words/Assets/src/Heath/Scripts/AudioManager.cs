dd377c8af8a0457bdbe6b67f58fa0b91b828a2df
heatht13 <77465843+heatht13@users.noreply.github.com>
2021-09-29 23:28:48 -0700

    Added Sounds
    Have some prefab sound objects and an audio object that holds an audio manager script

---
+++ Them's fightin' words/Assets/src/Heath/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //public AudioSource menuMusic;
+    //public AudioSource worldMusic;
+    //public AudioSource fightMusic;
+
+    public AudioSource jump;
+    public AudioSource pickup;
+
+    //play button sound
+    public void playJump()
+    {
+        jump.Play();
+    }
+
+
+    //play pickup sound
+   /* public void playPickup()
+    {
+        pickup.Play();
+    }*/
+
+}
