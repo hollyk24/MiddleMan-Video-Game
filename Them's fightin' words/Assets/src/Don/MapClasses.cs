3e223abc5aafc782874736b3f5cd2462034e82bb
SpartanDH45 <90274353+SpartanDH45@users.noreply.github.com>
2021-09-30 04:05:16 -0700

    Added Tile Classes

---
+++ Them's fightin' words/Assets/src/Don/MapClasses.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTile
+{
+    public bool hasCollision;
+    public int tileLayer;
+    //tileSprite
+}
+public class ObstTile : MapTile
+{
+    new public bool hasCollision = true;
+    new public int tileLayer = 1;
+}
+
+public class InteractTile: ObstTile
+{
+    public bool interactable = true;
+    public string interactionText;
+}
