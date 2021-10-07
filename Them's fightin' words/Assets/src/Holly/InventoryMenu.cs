56d5f2db934b6cb44e1f611b5be52dfa08776e87
hollyk24 <keir8638@vandals.uidaho.edu>
2021-10-06 18:13:37 -0700

    Instructions menu

---
+++ Them's fightin' words/Assets/src/Holly/InventoryMenu.cs
@@ -0,0 +1,20 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu
+{
+    private List<Item> itemList;
+
+    public Inventory(){
+        itemList = new List<Item>();
+
+        Debug.Log("Inventory");
+    }
+
+    public void AddItem(Item item){
+        itemList.Add(item);
+    }
+}
+*/
