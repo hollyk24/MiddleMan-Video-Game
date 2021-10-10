7c3f36483599776e8996e15f7100d879da857b88
selunders <sethlunders@gmail.com>
2021-10-10 15:47:59 -0700

    Fix movement bugs & align player to grid

--- Them's fightin' words/Assets/src/Seth/playerMovement.cs
+++ Them's fightin' words/Assets/src/Seth/playerMovement.cs
@@ -5,16 +5,20 @@ using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    private const bool V = false;
+    public float speed = 1f;
     public float speedMultiplier = 1;
     private Controls controls;
+    private Transform target;
+    private bool movementLock = false;
 
 
     InputAction UpInput, DownInput, LeftInput, RightInput, runToggleInput;
     private void Start()
     {
-        Debug.Log("In Start function");
+        // Debug.Log("In Start function");
         controls = GameManager.CONTROLS;
+
         UpInput = controls.Player.moveUp;
         DownInput = controls.Player.moveDown;
         LeftInput = controls.Player.moveLeft;
@@ -30,31 +34,53 @@ public class playerMovement : MonoBehaviour
 
     public void moveUp(InputAction.CallbackContext context)
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime * speedMultiplier);
+        if (context.started == true && movementLock != true){
+            // StopAllCoroutines();
+            movementLock = true;
+            StartCoroutine(movePlayerTowards(transform.position + new Vector3(0, 1, 0)));
+        }
         // Debug.Log(context);
         // Debug.Log("Moving Up");
     }
     public void moveDown(InputAction.CallbackContext context)
     {
-        transform.Translate(Vector3.down * speed * Time.deltaTime * speedMultiplier);
+        if (context.started == true && movementLock != true){
+            // StopAllCoroutines();
+            movementLock = true;
+            StartCoroutine(movePlayerTowards(transform.position + new Vector3(0, -1, 0)));
+        }
     }
     public void moveLeft(InputAction.CallbackContext context)
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime * speedMultiplier);
+        if (context.started == true && movementLock != true){
+            // StopAllCoroutines();
+            movementLock = true;
+            StartCoroutine(movePlayerTowards(transform.position + new Vector3(-1, 0, 0)));
+        }
     }
     public void moveRight(InputAction.CallbackContext context)
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime * speedMultiplier);
+        if (context.started == true && movementLock != true){
+            // StopAllCoroutines();
+            movementLock = true;
+            StartCoroutine(movePlayerTowards(transform.position + new Vector3(1, 0, 0)));
+        }
+    }
+
+    public IEnumerator movePlayerTowards(Vector3 end)
+    {
+        while (transform.position != end)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, end, speed * Time.deltaTime * speedMultiplier);
+            yield return null;
+        }
+        movementLock = false;
     }
     public void runToggle(InputAction.CallbackContext context)
     {
         if (speedMultiplier == 1)
-            speedMultiplier = 1.5F;
+            speedMultiplier = 2;
         else
             speedMultiplier = 1;
     }
-
-    // void Start(){
-    //     // transform.position = new Vector3(0,0,0);
-    // }
 }
