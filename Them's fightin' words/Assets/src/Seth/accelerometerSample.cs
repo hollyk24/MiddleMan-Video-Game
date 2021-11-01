d0abe018a2ed4f23b62f12ce181955c660f99d3e
selunders <sethlunders@gmail.com>
2021-11-01 16:24:16 -0700

    Working on getting controls working for android

---
+++ Them's fightin' words/Assets/src/Seth/accelerometerSample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class accelerometerSample : MonoBehaviour
+{
+    float speed = 20.0f;
+
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+        // we assume that the device is held parallel to the ground
+        // and the Home button is in the right hand
+
+        // remap the device acceleration axis to game coordinates:
+        // 1) XY plane of the device is mapped onto XZ plane
+        // 2) rotated 90 degrees around Y axis
+
+        dir.x = Input.acceleration.x;
+        dir.y = Input.acceleration.y;
+
+        // clamp acceleration vector to the unit sphere
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        // Make it move X meters per second instead of X meters per frame...
+        dir *= Time.deltaTime;
+
+        // Move object
+        transform.Translate(dir * speed);
+    }
+
+
+}
---
+++ Them's fightin' words/Assets/src/Seth/itemObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class itemObject : Interactable
+{
+    public int PickUpItemID = 01; // Let's make a sandwich
+}
