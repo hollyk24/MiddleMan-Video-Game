1f553e89fda3a1d12b4d3122844af626a36d1976
lily <lilymason@msn.com>
2021-11-02 18:19:51 -0700

    Holly Pause Screen

---
+++ Them's fightin' words/Assets/src/Holly/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+    private void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+
+            isPaused = !isPaused;
+           
+        }
+        if (isPaused) {
+            ActivateMenu();
+        }
+        else {
+            DeactivateMenu();
+        }
+    }
+
+    public void ActivateMenu() {
+
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu() {
+
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void QuitGame() {
+
+        Debug.Log("QUIT!");
+        AudioManager.Play(AudioLibrary.Library.Select);
+        Application.Quit();
+    }
+
+    public void SettingGame() {
+        SceneManager.LoadScene("SettingsMenu");
+        AudioManager.Play(AudioLibrary.Library.Select);
+        //Debug.Log("Settings");
+    }
+
+    public void InstructionsGame() {
+        //SceneManager.LoadScene("InstructionsMenu");
+        //Debug.Log("Instructions");
+    }
+
+}
+
