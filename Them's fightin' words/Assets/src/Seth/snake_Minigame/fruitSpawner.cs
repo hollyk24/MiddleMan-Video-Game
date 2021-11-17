878376633c698c01a4b3bec75d2d61bb07254814
selunders <sethlunders@gmail.com>
2021-11-17 02:06:31 -0800

    Snake Game is finished, may be too speedy...

---
+++ Them's fightin' words/Assets/src/Seth/snake_Minigame/fruitSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruitSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject snake_fruitPrefab;
+    [SerializeField] private GameObject snake_fruitPrefabYellow;
+    public bool spawnFruit = true;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // spawnFruit = true;
+        StartCoroutine(fruitSpawnerLoop());
+    }
+
+    // Update is called once per frame
+    public IEnumerator fruitSpawnerLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.25f);
+            if (spawnFruit)
+            {
+                spawnFruit = false;
+                float randNum = Mathf.Round(Random.Range(0, 10));
+                if (randNum < 7)
+                {
+                    Instantiate(snake_fruitPrefab, new Vector3(Mathf.Round(Random.Range(-8, 7)), Mathf.Round(Random.Range(-5, 4)), -4), Quaternion.identity, this.transform);
+
+                }
+                else
+                {
+                    Instantiate(snake_fruitPrefabYellow, new Vector3(Mathf.Round(Random.Range(-8, 7)), Mathf.Round(Random.Range(-5, 4)), -4), Quaternion.identity, this.transform);
+                }
+                // newFruit.transform.SetParent(this.transform);
+            }
+        }
+    }
+
+    public void fruitNeedsSpawned()
+    {
+        spawnFruit = true;
+    }
+}
--- Them's fightin' words/Assets/src/Seth/snake_Minigame/wallCollider.cs
+++ Them's fightin' words/Assets/src/Seth/snake_Minigame/wallCollider.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class wallCollider : MonoBehaviour
-{
-    public SnakeManager SM;
-    // public bool gameOver = false;
-    void Start() {
-        SM = transform.parent.transform.parent.GetComponent<SnakeManager>();
-    }
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        // Debug.Log("OnTriggerExit2D");
-        SM.GAMEOVER();
-        // GameObject.Find("snakeHead").GetComponent<SnakeHead>().StopCoroutine(movementListener());
-        // gameOverPanel.SetActive(true);
-    }
-}
+// public class wallCollider : MonoBehaviour
+// {
+//     SnakeManager SM;
+//     // public bool gameOver = false;
+//     void Start() {
+//         SM = transform.parent.transform.parent.GetComponent<SnakeManager>();
+//     }
+//     void OnTriggerEnter2D(Collider2D col)
+//     {
+//         // Debug.Log("OnTriggerExit2D");
+//         // SM.GAMEOVER();
+//         // GameObject.Find("snakeHead").GetComponent<SnakeHead>().StopCoroutine(movementListener());
+//         // gameOverPanel.SetActive(true);
+//     }
+// }
