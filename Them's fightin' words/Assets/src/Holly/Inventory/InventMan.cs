a6142e3ed141d08cc09dfdd8b4c86039226e5a9b
lily <lilymason@msn.com>
2021-11-16 20:52:47 -0800

    Inventory and Patterns (holly)

--- Them's fightin' words/Assets/src/Holly/Inventory/InventMan.cs
+++ Them's fightin' words/Assets/src/Holly/Inventory/InventMan.cs
@@ -7,7 +7,32 @@ public class InventMan : MonoBehaviour {
     [SerializeField] public GameObject InventoryHeart;
     [SerializeField] public GameObject InventorySnow;
     public List<GameObject> InventoryList;
+    private static InventMan instance;
 
+    public static InventMan Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<InventMan>();
+                if(instance == null) {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(InventMan).Name;
+                    instance = obj.AddComponent<InventMan>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake() {
+        
+        //Keep only one InventMan script
+        if (instance == null) {
+            instance = this as InventMan;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
     private void Start() {
             InventoryList = new List<GameObject> {
             InventoryApple,
