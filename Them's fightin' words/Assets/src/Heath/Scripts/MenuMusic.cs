c97e3ed6b6654a6c724f5b753fcfa98779cb8ede
heatht13 <77465843+heatht13@users.noreply.github.com>
2021-11-18 01:11:38 -0800

    Updated Audio
    Added Slider Feature
    Updated Code to meet requirements

--- Them's fightin' words/Assets/src/Heath/Scripts/MenuMusic.cs
+++ Them's fightin' words/Assets/src/Heath/Scripts/MenuMusic.cs
@@ -1,22 +1,27 @@
-﻿using System.Collections;
+﻿/*
+ * Filename: MenuMusic.cs
+ * Developer: Heath Thompson
+ * Purpose: This file stores the audio files used by the Audio Manager in Them's Fightin' Words
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * An empty class to demonstrate comments.
+ *
+ * Member variables:
+ * m_isClass -- Boolean to demonstrate a member variable comment.
+ */
 public class MenuMusic : MonoBehaviour
 {
+
     private void Awake()
     {
         AudioManager.Loop(AudioLibrary.Library.Menu1);
     }
-    // Start is called before the first frame update
-    /*void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }*/
 }
+
+
---
+++ Them's fightin' words/Assets/src/Heath/Scripts/ObjectPool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectPool : MonoBehaviour
+{
+    private static int PoolSize = 8;
+    private static readonly Queue Pool = new Queue(PoolSize);
+
+    public GameObject GetObject()
+    {
+        GameObject Obj = null;
+
+        if(Pool.Count > 0)
+        {
+            Obj = (GameObject)Pool.Dequeue();
+        }
+        else
+        {
+            if(Pool.Count >= PoolSize)
+            {
+                Debug.Log("The object pool is full. Please reuse another object.");
+            }
+            else
+            {
+                Obj = new GameObject("Pool Object");
+                Obj.tag = "Pool Object";
+                Obj.AddComponent<AudioSource>();
+            }
+           
+        }
+        return Obj;
+    }
+
+    public void RecycleObject(GameObject RecycledObject)
+    {
+        RecycledObject.GetComponent<AudioSource>().clip = null;
+        Pool.Enqueue(RecycledObject);
+    }
+
+}
