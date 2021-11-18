86991e7c551d2b8953a4a5802c20109ca13a3934
SpartanDH45 <90274353+SpartanDH45@users.noreply.github.com>
2021-11-18 11:41:52 -0800

    Revert "Revert "Revert "Teleporter Update"""
    This reverts commit fc179f638b5b3583d38669fc4c3c00a4f216ed4c.

--- Them's fightin' words/Assets/src/Don/TeleporterTile.cs
+++ Them's fightin' words/Assets/src/Don/TeleporterTile.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class TeleporterTile : SpecialTiles
 {
     TeleporterTile()
     {
-        m_tileQualities = "This tile is a teleporter. ";
+        tileQualities = "This tile is a teleporter. ";
     }
     [SerializeField] public Transform tTarget;
-    public override void TileEffect(Collider2D coll)
+    public override void tileEffect(Collider2D coll)
     {
         coll.gameObject.transform.position = (tTarget.position + new Vector3(0f, 0.5f, 0f));
-        //Debug.Log(coll.gameObject.transform.position.ToString());
+        Debug.Log(coll.gameObject.transform.position.ToString());
     }
 }
