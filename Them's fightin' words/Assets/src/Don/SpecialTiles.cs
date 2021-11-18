67e2f0cda05e27cab1f01f80bdda751166e76a39
SpartanDH45 <90274353+SpartanDH45@users.noreply.github.com>
2021-11-17 20:49:00 -0800

    Added Teleporter

---
+++ Them's fightin' words/Assets/src/Don/SpecialTiles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialTiles : MonoBehaviour
+{
+    public IEnumerator EffectDelay(Collider2D coll)
+    {
+        Debug.Log("Before Wait");
+        yield return new WaitForSeconds(0.2f);
+        tileEffect(coll);
+    }
+    public virtual void tileEffect (Collider2D coll){
+        Debug.Log("This tile seems very special.");
+    }
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        StartCoroutine(EffectDelay(coll));
+    }
+}
---
+++ Them's fightin' words/Assets/src/Don/TeleporterTile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterTile : SpecialTiles
+{
+    [SerializeField] public Transform tTarget;
+    public override void tileEffect(Collider2D coll)
+    {
+        coll.gameObject.transform.position = (tTarget.position + new Vector3(0f, 0.5f, 0f));
+        Debug.Log(coll.gameObject.transform.position.ToString());
+    }
+}
