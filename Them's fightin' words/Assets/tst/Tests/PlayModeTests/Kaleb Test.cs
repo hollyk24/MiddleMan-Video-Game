c34db05ce4824f240df277c7ca81cd1d9ef23403
Kaleb Johnson <kalebjl@gmail.com>
2021-12-06 07:00:38 -0800

    Updating some scenes and tests

--- Them's fightin' words/Assets/tst/Tests/PlayModeTests/Kaleb Test.cs
+++ Them's fightin' words/Assets/tst/Tests/PlayModeTests/Kaleb Test.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using NUnit.Framework;
+using UnityEditor;
+using FightCharacter;
 
 namespace Tests
 {
     public class KalebTest
     {
+        private GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/Kaleb/TestNPC.prefab");
+
         public void loadTScene() {
             SceneManager.LoadScene("FGTestScene");
         }
@@ -16,14 +20,22 @@ namespace Tests
         [UnityTest]
         /*Tests the number of simultaneous enemies that can exist under the current system*/
         public IEnumerator NPCStressTest() {
+            List<GameObject> TestNPCs = new List<GameObject>(); 
             loadTScene();
             yield return new WaitForSeconds(1);
-
-            var NPC = GameObject.FindGameObjectWithTag("NPC");
             int count = 0;
             while((int)(1f / Time.unscaledDeltaTime) > 30) {
-                GameObject.Instantiate(NPC);
+                //Debug.Log("Loop "+count);
+                TestNPCs.Add(GameObject.Instantiate(prefab));
+                Debug.Log("NPC Created: "+ count);
+                /*Debug.Log("NPC x, y" + newNPC.transform.position.x + ", "+ newNPC.transform.position.x );
+                newNPC.GetComponent<NPC>().enemy = GameObject.Find("Player").GetComponent<Player>();
+                newNPC.GetComponent<NPC>().master = GameObject.Find("Manager").GetComponent<FGManager>();
+                Debug.Log("Added Enemy: "+count);*/
+                //NPCs.Add();
+                //
                 count++;
+                
                 yield return null;
             }
             Debug.Log("NPCs spawned by stress test: " + count);
@@ -33,6 +45,6 @@ namespace Tests
         }
         
 
-        T
+        
     }
 }
